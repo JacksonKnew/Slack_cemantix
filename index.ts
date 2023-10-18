c63b2bf789a0dee1f868df3aa04ff9d068e20c6a
JacksonKnew <paullestrat@hotmail.fr>
2023-10-18 16:06:47 +0200

    calculates similarity papa maman

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
+
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+  
+    const url = "http://nlp.polytechnique.fr/similarityscore"
+    const data = {
+        "lang": "fr",
+        "sim1": "papa",
+        "sim2": "maman",
+        "type": "General Word2Vec"
+    }
+
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data)
+    });
+    const r_data = await response.json();
+  
+    return new Response(r_data.simscore);
 }
 
 serve(handler);
